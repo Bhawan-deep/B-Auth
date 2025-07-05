5ab785068b090d324cc9503b68c5c1f8e4209cbb
bhawandeep <bhawandeepsingh976@gmail.com>
2025-07-05 16:57:10 +0530

    redirect

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
      tailwindcss(),
-    react()],
+    react(), viteStaticCopy({
+      targets: [
+        { src: 'public/_redirects', dest: '.' } // this ensures _redirects goes to dist/
+      ]
+    })],
+    
      base: '/',
 })
